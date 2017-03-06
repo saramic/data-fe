f433838485a89a14ccc296d79c6d43a6874c79c0
Michael Milewski <michael.milewski@redbubble.com>
2017-03-06 22:01:11 +1100

    changed this to be share price but still hardcoded

--- src/shared/components/Gallery/GraphD3/GraphD3.js
+++ src/shared/components/Gallery/GraphD3/GraphD3.js
@@ -2,6 +2,71 @@ import React from 'react';
 import { LineChart } from 'react-d3-basic';
 
 function GraphD3() {
+  const chartData = [
+    {
+      Date: "2017-03-03",
+      Open: "21.16",
+      High: "21.35",
+      Low: "21.03",
+      Close: "21.10",
+      Volume: "4647100",
+      'Adj Close': "21.0019"
+    },
+    {
+      Date: "2017-03-02",
+      Open: "21.50",
+      High: "22.01",
+      Low: "21.50",
+      Close: "21.75",
+      Volume: "2747900",
+      'Adj Close': "21.64887"
+    },
+    {
+      Date: "2017-03-01",
+      Open: "22.06",
+      High: "22.21",
+      Low: "21.53",
+      Close: "21.83",
+      Volume: "3087500",
+      'Adj Close': "21.7285"
+    },
+    {
+      Date: "2017-02-28",
+      Open: "22.36",
+      High: "22.47",
+      Low: "21.98",
+      Close: "22.14",
+      Volume: "4572100",
+      'Adj Close': "22.03706"
+    },
+    {
+      Date: "2017-02-27",
+      Open: "23.70",
+      High: "23.71",
+      Low: "23.01",
+      Close: "23.07",
+      Volume: "2708300",
+      'Adj Close': "22.96274"
+    },
+    {
+      Date: "2017-02-24",
+      Open: "23.85",
+      High: "23.99",
+      Low: "23.35",
+      Close: "23.53",
+      Volume: "5134100",
+      'Adj Close': "23.4206"
+    },
+    {
+      Date: "2017-02-23",
+      Open: "23.00",
+      High: "23.47",
+      Low: "22.87",
+      Close: "23.42",
+      Volume: "3302900",
+      'Adj Close': "23.31111"
+    }
+  ];
   const width = 700;
   const height = 300;
   const margins = {left: 100, right: 100, top: 50, bottom: 50};
@@ -12,53 +77,16 @@ function GraphD3() {
     // color: what color is the line
   const chartSeries = [
     {
-       field: 'BMI',
-       name: 'BMI',
-       color: '#ff7f0e'
-     }
-   ];
-    // your x accessor
+      field: 'Close',
+      name: 'Close',
+      color: '#ff7f0e'
+    }
+  ];
+  // your x accessor
   const x = function(d) {
-    return d.index;
+    return new Date(d.Date);
   }
-  const chartData = [
-     {
-        name: "Lavon Hilll I",
-        BMI: 20.57,
-        age: 12,
-        birthday: "1994-10-26T00:00:00.000Z",
-        city: "Annatown",
-        married: true,
-        index: 1
-      },
-      {
-        name: "Clovis Pagac",
-        BMI: 24.28,
-        age: 26,
-        birthday: "1995-11-10T00:00:00.000Z",
-        city: "South Eldredtown",
-        married: false,
-        index: 3
-      },
-      {
-        name: "Gaylord Paucek",
-        BMI: 24.41,
-        age: 30,
-        birthday: "1975-06-12T00:00:00.000Z",
-        city: "Koeppchester",
-        married: true,
-        index: 5
-      },
-      {
-        name: "Ashlynn Kuhn MD",
-        BMI: 23.77,
-        age: 32,
-        birthday: "1985-08-09T00:00:00.000Z",
-        city: "West Josiemouth",
-        married: false,
-        index: 6
-      },
-    ];
+  const xScale = 'time'
   return (
       <div>
         <LineChart
@@ -69,6 +97,7 @@ function GraphD3() {
           height={height}
           chartSeries={chartSeries}
           x={x}
+          xScale={xScale}
         />
       </div>
   );
