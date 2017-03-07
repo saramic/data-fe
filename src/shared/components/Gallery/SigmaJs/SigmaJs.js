a946cfa132ed46ba8bc4fa04fe9b6430b088586f
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2017-03-07 19:41:44 +1100

    Making the smallest version of SigmaJS work

--- src/shared/components/Gallery/SigmaJs/SigmaJs.js
+++ src/shared/components/Gallery/SigmaJs/SigmaJs.js
@@ -1,11 +1,71 @@
 import React from 'react';
 
-function SigmaJs() {
-  return (
-    <div>
-      <a href="http://sigmajs.org/">SigmaJs</a> is an option in the graph space
-    </div>
-  );
+const graph = {
+  "nodes": [
+    {
+      "id": "n0",
+      "label": "A node",
+      "x": 0,
+      "y": 0,
+      "size": 3
+    },
+    {
+      "id": "n1",
+      "label": "Another node",
+      "x": 3,
+      "y": 1,
+      "size": 2
+    },
+    {
+      "id": "n2",
+      "label": "And a last one",
+      "x": 1,
+      "y": 3,
+      "size": 1
+    }
+  ],
+  "edges": [
+    {
+      "id": "e0",
+      "source": "n0",
+      "target": "n1"
+    },
+    {
+      "id": "e1",
+      "source": "n1",
+      "target": "n2"
+    },
+    {
+      "id": "e2",
+      "source": "n2",
+      "target": "n0"
+    }
+  ]
+};
+
+class SigmaJs extends React.Component {
+  constructor() {
+    super();
+    this.state = { isServer: true };
+  }
+
+  componentDidMount() {
+    this.setState({ isServer: false });
+  }
+
+  get sigmaComponent() {
+    const { Sigma, RandomizeNodePositions, RelativeSize } = require('react-sigma');
+    return <Sigma graph={graph} settings={{drawEdges:true}}> <RelativeSize initialSize={15}/></Sigma>;
+  }
+
+  render() {
+    return (
+      <div>
+        <a href="http://sigmajs.org/">SigmaJs</a> is an option in the graph space
+        { !this.state.isServer && this.sigmaComponent }
+      </div>
+    );
+  }
 }
 
 export default SigmaJs;
